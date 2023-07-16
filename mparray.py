2b094348183f8cb5e4e392ae356b900e0210ff39
Matt Haberland <mhaberla@calpoly.edu>
2023-07-16 00:09:02 -0500

    ENH: add more special functions

--- mparray.py
+++ mparray.py
@@ -66,6 +66,43 @@ for f in creation_funcs.split():
 # Data Type Functions need to be defined manually
 # Data Types to be defined
 
+
+# need to define these
+# add
+# bitwise_and
+# bitwise_left_shift
+# bitwise_invert
+# bitwise_or
+# bitwise_right_shift
+# bitwise_xor
+# divide
+# equal
+# floor_divide
+# greater
+# greater_equal
+# less
+# less_equal
+# log2
+# logaddexp
+# logical_and
+# logical_not
+# logical_or
+# logical_xor
+# multiply
+# negative
+# not_equal
+# positive
+# pow
+# remainder
+# round
+# square
+# subtract
+# trunc
+
+abs = np.vectorize(abs)
+real = np.vectorize(mp.re)
+imag = np.vectorize(mp.im)
+
 # Elementwise Functions
 elementwise_funcs = """
 abs
@@ -191,17 +228,50 @@ def double_working_precision(f):
     return wrapped
 
 
+def working_precision_for_complement(f):
+    def wrapped(x, *args, **kwargs):
+        extra_dps = int(mp.ceil(-mp.log10(x)))
+        mp.dps += extra_dps
+        res = f(x, *args, **kwargs)
+        mp.dps -= extra_dps
+        return res
+
+    return wrapped
+
+
+# def complement(f, sign=1):
+#     def wrapped(*args, **kwargs):
+#         res = f(*args, **kwargs)
+#         extra_dps = int(mp.ceil(-mp.log10(res)))
+#         mp.dps += extra_dps
+#         res = mp.one - res
+#         mp.dps -= extra_dps
+#         return res * sign
+#
+#     return wrapped
+
+
+
 # Move these to a `special` namespace
 expm1 = np.vectorize(mp.expm1)
 log1p = np.vectorize(mp.log1p)
 factorial2 = np.vectorize(mp.fac2)
 psi = np.vectorize(mp.digamma)
-digamma = np.vectorize(mp.digamma)
+digamma = psi
 ndtr = np.vectorize(mp.ncdf)
 gammaln = np.vectorize(mp.loggamma)
 erf = np.vectorize(mp.erf)
 erfc = np.vectorize(mp.erfc)
 zeta = np.vectorize(mp.zeta)
+poch = np.vectorize(mp.rf)
+binom = np.vectorize(mp.binomial)
+comb = binom
+lambertw = np.vectorize(mp.lambertw)
+powm1 = np.vectorize(mp.powm1)
+hyp1f1 = np.vectorize(mp.hyp1f1)
+hyp2f1 = np.vectorize(mp.hyp2f1)
+iv = np.vectorize(mp.besseli)
+kv = np.vectorize(mp.besselk)
 
 
 @np.vectorize
@@ -218,21 +288,19 @@ def gammaincc(x, a):
 def ndtri(x):
     extra_dps = int(mp.ceil(-mp.log10(x)))
     mp.dps += extra_dps
-    res = mp.sqrt(2)*mp.erfinv(2*x - mp.one)
+    res = mp.sqrt(2) * mp.erfinv(2 * x - mp.one)
     mp.dps -= extra_dps
     return res
 
 
 @np.vectorize
-@double_working_precision  # not always sufficient
 def log_ndtr(x):
-    return mp.log(mp.ncdf(x))
+    return mp.log(mp.ncdf(x))  # needs extra precision for large x
 
 
 @np.vectorize
-@double_working_precision  # not always sufficient
 def betaln(x, y):
-    return mp.log(mp.beta(x, y))
+    return mp.log(mp.beta(x, y))  # may need extra precision
 
 
 @np.vectorize
@@ -245,6 +313,73 @@ def xlog1py(x, y):
     return x*log1p(y)
 
 
+@np.vectorize
+def cosm1(x):
+    # second term in cosine series is x**2/2
+    extra_dps = 2*int(mp.ceil(-mp.log10(x))) + 1
+    mp.dps += extra_dps
+    res = mp.cos(x) - mp.one
+    mp.dps -= extra_dps
+    return res
+
+
+@np.vectorize
+def logit(x):
+    res = mp.log(x) - mp.log1p(-x)  # needs precision near x=0.5
+    return res
+
+
+@np.vectorize
+def expit(x):
+    return mp.exp(x - mp.log1p(mp.exp(x)))  # needs extra precision
+
+
+@np.vectorize
+def boxcox(x, lmbda):  # precision check
+    """
+    y = (x**lmbda - 1) / lmbda  if lmbda != 0
+        log(x)                  if lmbda == 0
+    """
+    if lmbda != 0:
+        return mp.powm1(x, lmbda) / lmbda
+    else:
+        return mp.lmbda(x)
+
+
+def boxcox1p(x, lmbda):  # precision check
+    return boxcox(1 + x, lmbda)
+
+
+def logsumexp(x):
+    # as far as I know, logsumexp is to avoid overflow,
+    # not to improve precision, and mpmath avoids overflow
+    return mp.log(exp(x).sum())  # noqa
+
+
+def ive(v, z):
+    return iv(v, z) * exp(-abs(real(z)))  # noqa
+
+
+def i0e(x):
+    return ive(0, x)
+
+
+def i1e(x):
+    return ive(1, x)
+
+
+def kve(v, z):
+    return kv(v, z) * exp(z)  # noqa
+
+
+def k0e(x):
+    return kve(0, x)
+
+
+def k1e(x):
+    return kve(1, x)
+
+
 # others to be added
 # gammaincinv
 # gammainccinv
@@ -253,31 +388,14 @@ def xlog1py(x, y):
 # chdtri
 # chndtr
 # chndtrix
-# ive
-# i0e
-# i1e
-# hyp1f1
-# hyp2f1
 # stdtr
 # stdtrit
-# powm1
-# logsumexp
 # ndtri_exp
 # tklmbda
-# boxcox
-# boxcox1p
 # inv_boxcox
 # inv_boxcox1p
-# cosm1
-# binom
 # kolmogorov, smirnov, f
-# comb
-# lambertw
-# poch
-# kv
-# kve
-# k1e
-# expit
-# logit
 # erfcinv
 # erfinv
+
+

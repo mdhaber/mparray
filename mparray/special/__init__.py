a24107709dff4c66630b9aa9bb90e1be4991b276
Matt Haberland <mhaberla@calpoly.edu>
2023-07-16 00:23:30 -0500

    MAINT: split special into subpackage

---
+++ mparray/special/__init__.py
@@ -0,0 +1,1 @@
+from ._special import *
---
+++ mparray/special/_special.py
@@ -0,0 +1,182 @@
+import numpy as np
+from mpmath import mp
+from mparray import exp, real
+
+
+def double_working_precision(f):
+    def wrapped(*args, **kwargs):
+        mp.dps *= 2
+        res = f(*args, **kwargs)
+        mp.dps //= 2
+        return res
+
+    return wrapped
+
+
+def working_precision_for_complement(f):
+    def wrapped(x, *args, **kwargs):
+        extra_dps = int(mp.ceil(-mp.log10(x)))
+        mp.dps += extra_dps
+        res = f(x, *args, **kwargs)
+        mp.dps -= extra_dps
+        return res
+
+    return wrapped
+
+
+# def complement(f, sign=1):
+#     def wrapped(*args, **kwargs):
+#         res = f(*args, **kwargs)
+#         extra_dps = int(mp.ceil(-mp.log10(res)))
+#         mp.dps += extra_dps
+#         res = mp.one - res
+#         mp.dps -= extra_dps
+#         return res * sign
+#
+#     return wrapped
+
+
+expm1 = np.vectorize(mp.expm1)
+log1p = np.vectorize(mp.log1p)
+factorial2 = np.vectorize(mp.fac2)
+psi = np.vectorize(mp.digamma)
+digamma = psi
+ndtr = np.vectorize(mp.ncdf)
+gammaln = np.vectorize(mp.loggamma)
+erf = np.vectorize(mp.erf)
+erfc = np.vectorize(mp.erfc)
+zeta = np.vectorize(mp.zeta)
+poch = np.vectorize(mp.rf)
+binom = np.vectorize(mp.binomial)
+comb = binom
+lambertw = np.vectorize(mp.lambertw)
+powm1 = np.vectorize(mp.powm1)
+hyp1f1 = np.vectorize(mp.hyp1f1)
+hyp2f1 = np.vectorize(mp.hyp2f1)
+iv = np.vectorize(mp.besseli)
+kv = np.vectorize(mp.besselk)
+
+
+@np.vectorize
+def gammainc(x, a):
+    return mp.gammainc(x, a=0, b=a, regularized=True)
+
+
+@np.vectorize
+def gammaincc(x, a):
+    return mp.gammainc(x, a=a, b=mp.inf, regularized=True)
+
+
+@np.vectorize
+def ndtri(x):
+    extra_dps = int(mp.ceil(-mp.log10(x)))
+    mp.dps += extra_dps
+    res = mp.sqrt(2) * mp.erfinv(2 * x - mp.one)
+    mp.dps -= extra_dps
+    return res
+
+
+@np.vectorize
+def log_ndtr(x):
+    return mp.log(mp.ncdf(x))  # needs extra precision for large x
+
+
+@np.vectorize
+def betaln(x, y):
+    return mp.log(mp.beta(x, y))  # may need extra precision
+
+
+@np.vectorize
+def xlogy(x, y):
+    return x*mp.log(y)
+
+
+@np.vectorize
+def xlog1py(x, y):
+    return x*log1p(y)
+
+
+@np.vectorize
+def cosm1(x):
+    # second term in cosine series is x**2/2
+    extra_dps = 2*int(mp.ceil(-mp.log10(x))) + 1
+    mp.dps += extra_dps
+    res = mp.cos(x) - mp.one
+    mp.dps -= extra_dps
+    return res
+
+
+@np.vectorize
+def logit(x):
+    res = mp.log(x) - mp.log1p(-x)  # needs precision near x=0.5
+    return res
+
+
+@np.vectorize
+def expit(x):
+    return mp.exp(x - mp.log1p(mp.exp(x)))  # needs extra precision
+
+
+@np.vectorize
+def boxcox(x, lmbda):  # precision check
+    """
+    y = (x**lmbda - 1) / lmbda  if lmbda != 0
+        log(x)                  if lmbda == 0
+    """
+    if lmbda != 0:
+        return mp.powm1(x, lmbda) / lmbda
+    else:
+        return mp.lmbda(x)
+
+
+def boxcox1p(x, lmbda):  # precision check
+    return boxcox(1 + x, lmbda)
+
+
+def logsumexp(x):
+    # as far as I know, logsumexp is to avoid overflow,
+    # not to improve precision, and mpmath avoids overflow
+    return mp.log(exp(x).sum())
+
+
+def ive(v, z):
+    return iv(v, z) * exp(-abs(real(z)))
+
+
+def i0e(x):
+    return ive(0, x)
+
+
+def i1e(x):
+    return ive(1, x)
+
+
+def kve(v, z):
+    return kv(v, z) * exp(z)
+
+
+def k0e(x):
+    return kve(0, x)
+
+
+def k1e(x):
+    return kve(1, x)
+
+
+# others to be added
+# gammaincinv
+# gammainccinv
+# chdtr
+# chdtrc
+# chdtri
+# chndtr
+# chndtrix
+# stdtr
+# stdtrit
+# ndtri_exp
+# tklmbda
+# inv_boxcox
+# inv_boxcox1p
+# kolmogorov, smirnov, f
+# erfcinv
+# erfinv
